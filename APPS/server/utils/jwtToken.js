af3adf975fa3a4dfe084ac2e5b62de4e452e21ff
Jul-JY <julietta198307@gmail.com>
2022-12-05 17:18:05 +0800

    creating jwt

---
+++ APPS/server/utils/jwtToken.js
@@ -0,0 +1,22 @@
+// Creating Token And Save It In Cookie
+
+const sendToken = (user, status, res) => {
+  const token = user.getJWTToken();
+
+  // options for cookie
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  res.status(status).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+
+module.exports = sendToken;
