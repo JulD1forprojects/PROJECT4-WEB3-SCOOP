a7ede681c59b7ed643d20bc99d8343b9f4900ab1
Jul-JY <julietta198307@gmail.com>
2022-12-05 17:15:43 +0800

    creating userController

---
+++ APPS/server/controllers/userController.js
@@ -0,0 +1,147 @@
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+// ! Register a User
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    sendToken(user, 201, res);
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Error! Please Try Again Later",
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user has given pass and given
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Please Enter Email And Password" });
+    }
+
+    // get user from database
+    const user = await User.findOne({ email }).select("+password");
+
+    // if there is no user return an error
+    if (!user) {
+      return res
+        .status(401)
+        .send({ success: false, message: "Invalid Email or Password" });
+    }
+
+    // check if given password and password in database is the same
+    const isPasswordMatched = await user.comparePassword(password);
+
+    // if password is not same return an error
+    if (!isPasswordMatched) {
+      return res
+        .status(401)
+        .send({ success: false, message: "Invalid Email or Password" });
+    }
+
+    // retrurn token when all successful
+    sendToken(user, 200, res);
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Error! Please Try Again Later",
+    });
+  }
+};
+
+// ! Logout
+
+exports.logout = async (req, res) => {
+  try {
+    // delete token from cookie
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged Out",
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Error! Please Try Again Later",
+    });
+  }
+};
+
+// ! Get User By Id
+
+exports.getUserDetails = async (req, res) => {
+  try {
+    let user = await User.findById(req.user.id).select("+password");
+
+    res.status(200).json({
+      status: true,
+      user: user,
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Error! Please Try Again Later",
+    });
+  }
+};
+
+// ! Update Password
+
+exports.updatePassword = async (req, res, next) => {
+  let user = await User.findById(req.user.id).select("+password");
+
+  const isPasswordMatched = user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old Password is Incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHandler("New Password Does Not Match", 400));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  sendToken(user, 200, res);
+};
+
+//! Update Profile
+
+exports.updateProfile = async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Update Successfully",
+    user: user,
+  });
+};
