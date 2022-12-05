4662a4fd0d3a99e3ed36589bce977aa3c3ac1d43
Jul-JY <julietta198307@gmail.com>
2022-12-05 17:44:14 +0800

    Creating  isAuthenticated for user authntication and token verification

---
+++ APPS/server/middlewares/isAuthenticated.js
@@ -0,0 +1,33 @@
+// importing jwt for creating token
+const jwt = require("jsonwebtoken");
+
+// importing user model for referencing user
+const User = require("../models/userModel");
+
+exports.isAuthenticatedUser = async (req, res, next) => {
+  try {
+    // getting token from cookie
+    const { token } = req.cookies;
+
+    // if no user, return error to get login
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Please Login To Access This Resource",
+      });
+    }
+
+    // verify token
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    // return user profile
+    req.user = await User.findById(decodedData.id);
+
+    next();
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "You Are Not Authenticated",
+    });
+  }
+};
