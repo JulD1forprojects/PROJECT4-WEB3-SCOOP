2389686b856d2f2fb4795b882689051c7804745f
Jul-JY <julietta198307@gmail.com>
2022-12-08 18:19:52 +0800

    FavoriteCard Component rendering

--- APPS/client/src/components/FavoriteCard.jsx
+++ APPS/client/src/components/FavoriteCard.jsx
@@ -1,7 +1,20 @@
 import React from "react"; // react
 
 const ArticleCard = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      {/* article card */}
+      <div className="col-md-12">
+        <div className="card">
+          <div className="card-content">
+            <p className="description"></p>
+            <br />
+            <button className="btn btn-danger">Remove Favorite</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ArticleCard;
