4e2ea260dd3f35130858902aa1bb9bbd59938114
Jul-JY <julietta198307@gmail.com>
2022-12-08 16:47:10 +0800

    Rendering Categories page

--- APPS/client/src/pages/Categories.jsx
+++ APPS/client/src/pages/Categories.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/Header"; // header
+import Footer from "../components/Footer"; // footer
 
 const Categories = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <Header />
+      <br />
+      <h2 className="text-center">All Categories</h2>
+      <br />
+      <Container>
+        <div className="row"></div>
+      </Container>
+      <br />
+      <Footer />
+    </>
+  );
 };
 
 export default Categories;
