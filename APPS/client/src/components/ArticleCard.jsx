23222cf8beb00986e35e05511dd8ce998cb3786a
Jul-JY <julietta198307@gmail.com>
2022-12-08 18:15:32 +0800

    ArticleCard Component rendering

--- APPS/client/src/components/ArticleCard.jsx
+++ APPS/client/src/components/ArticleCard.jsx
@@ -1,7 +1,22 @@
 import React from "react"; // react
 
 const ArticleCard = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      {/* article card */}
+      <div className="col-md-12">
+        <div className="card">
+          <div className="card-content">
+            {/* article description */}
+            <h2>{article.title}</h2>
+            <p className="description"></p>
+            <br />
+            <button className="btn btn-myprimary">Add To Favorite</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ArticleCard;
