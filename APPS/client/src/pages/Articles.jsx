b8fcb146fbc75a68e69714421be912b09f5026f9
Jul-JY <julietta198307@gmail.com>
2022-12-09 00:51:48 +0800

    Articles, Categories Pages Update

--- APPS/client/src/pages/Articles.jsx
+++ APPS/client/src/pages/Articles.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import Header from "../components/Header"; // header
+import ArticleCard from "../components/ArticleCard"; // article card
+import { Container } from "react-bootstrap"; // conteiner for ui
 import Footer from "../components/Footer"; // footer
+import { useAppContext } from "../context/UseAppContext"; // app contexxt for getting articles
+const Articles = ({ user }) => {
+  const { articles } = useAppContext(); // getting articles
 
-const Articles = () => {
+  // rendering ui
   return (
     <>
-      <Header user />
+      <Header user={user} />
       <br />
       <h2 className="text-center">All Articles</h2>
       <br />
       <Container>
-        <div className="row"></div>
+        <div className="row">
+          {/* rendering  article card for all articles */}
+
+          {articles.map((v, i) => {
+            return <ArticleCard key={i} article={v} />;
+          })}
+        </div>
       </Container>
       <br />
       <Footer />
     </>
   );
 };
+
 export default Articles;
--- APPS/client/src/pages/Categories.jsx
+++ APPS/client/src/pages/Categories.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import Header from "../components/Header"; // header
-import Footer from "../components/Footer"; // footer
+import React, { useState } from "react";
+import Header from "../components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "../components/Footer";
+import { useAppContext } from "../context/UseAppContext";
 
-const Categories = () => {
+import CategoryCard from "../components/CategoryCard";
+const Categories = ({ user }) => {
+  const { categories } = useAppContext(); // getting categories from app state
+
+  // rendering ui
   return (
     <>
-      <Header />
+      <Header user={user} />
       <br />
       <h2 className="text-center">All Categories</h2>
       <br />
       <Container>
-        <div className="row"></div>
+        <div className="row">
+          {categories &&
+            categories.map((v, i) => {
+              return (
+                <div className="col-md-3">
+                  <CategoryCard key={i} category={v} />
+                </div>
+              );
+            })}
+        </div>
       </Container>
       <br />
       <Footer />
