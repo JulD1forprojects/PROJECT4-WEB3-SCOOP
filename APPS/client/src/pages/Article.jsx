224c38d66af0f2be91ce2ecd26488fd74c6835ec
Jul-JY <julietta198307@gmail.com>
2022-12-08 16:40:53 +0800

    rendering article page

--- APPS/client/src/pages/Article.jsx
+++ APPS/client/src/pages/Article.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Header from "../components/Header"; // header
+import Footer from "../components/Footer"; // footer
 
 const Article = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <Header />
+      <br />
+      <br />
+      <br />
+      {/* Main item container */}
+      <main className="articleitem">
+        <section className="img">
+          <img src={article.image} alt="Not Found" />
+        </section>
+        <section className="articleprice">
+          <h2 className="price-sub__heading">{article.category.category}</h2>
+          <h1 className="price-main__heading">{article.name}</h1>
+          <p className="price-txt" dangerouslySetInnerHTML></p>
+
+          <button
+            onClick={add}
+            className="btn price-cart__btn btn--orange"
+            style={{ height: "40px", background: "#7635f0", color: "white" }}
+          >
+            Add to favorites
+          </button>
+        </section>
+      </main>
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </>
+  );
 };
 
 export default Article;
