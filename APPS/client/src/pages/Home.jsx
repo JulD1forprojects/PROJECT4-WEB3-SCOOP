8c10debfc9b2df055175e53a71efd000589be9c2
Jul-JY <julietta198307@gmail.com>
2022-12-08 17:00:39 +0800

    Rendering home page

--- APPS/client/src/pages/Home.jsx
+++ APPS/client/src/pages/Home.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Header from "../components/Header"; // header
+import Footer from "../components/Footer"; // footer
 
 const Home = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <Header />
+      <div className="mmmmain">
+        <Slider />
+        <br />
+
+        <Container>
+          <div className="row">
+            <div className="col-md-7">
+              <h2 className="text-left" style={{ textAlign: "left" }}>
+                Featured Articles
+              </h2>
+              <br />
+
+              <div className="row"></div>
+            </div>
+
+            <div className="col-md-5">
+              <h2 className="text-center">Categories</h2>
+              <br />
+              <div className="row"></div>
+            </div>
+          </div>
+        </Container>
+        <br />
+
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Home;
