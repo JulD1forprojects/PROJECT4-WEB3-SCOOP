2a185033247282af932d2c6f24909afee40f5345
Jul-JY <julietta198307@gmail.com>
2022-12-08 16:53:14 +0800

    Rendering favorites page

--- APPS/client/src/pages/Favorites.jsx
+++ APPS/client/src/pages/Favorites.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/Header"; // header
+import Footer from "../components/Footer"; // footer
 
 const Favorites = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <Header />
+      <br />
+      <h2 className="text-center">My Favorites Blogs</h2>
+      <br />
+      <Container>
+        <div className="row"></div>
+      </Container>
+      <br />
+      <Footer />
+    </>
+  );
 };
 
 export default Favorites;
