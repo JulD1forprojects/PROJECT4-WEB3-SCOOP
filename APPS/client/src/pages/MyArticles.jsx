5550163898cf498dc14042a3f7904a9f2ab644b1
Jul-JY <julietta198307@gmail.com>
2022-12-08 17:24:07 +0800

    Partial Rendering of MyArticles Page

--- APPS/client/src/pages/MyArticles.jsx
+++ APPS/client/src/pages/MyArticles.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Header from "../components/Header"; // header
 
-const OrderDetail = () => {
-  return <div>reference</div>;
+const MyArticles = () => {
+  return (
+    <>
+      <Header />
+
+      <Container>
+        <br />
+        <div className="d-flex justify-content-between align-items-center gap-10 flex-wrap-wrap">
+          <h2 className="text-center">My Articles</h2>
+          <button className="btn btn-myprimary">Add New Article</button>
+        </div>
+        <br />
+
+        <div className="row">
+          <p className="description"></p>
+
+          <br />
+          <br />
+          <button className="btn btn-myprimary">Edit</button>
+
+          <button className="btn btn-myprimary">Delete</button>
+        </div>
+      </Container>
+      <br />
+      <Footer />
+    </>
+  );
 };
 
-export default OrderDetail;
+export default MyArticles;
