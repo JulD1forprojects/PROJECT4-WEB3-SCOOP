f528b489f49332341be953ffde08e4acad1bb544
Jul-JY <julietta198307@gmail.com>
2022-12-08 16:49:55 +0800

    Rendering CategoryArticles Page

--- APPS/client/src/pages/CategoryArticles.jsx
+++ APPS/client/src/pages/CategoryArticles.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/Header"; // header
+import Footer from "../components/Footer"; // footer
 
 const CategoryArticles = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <Header />
+      <br />
+      <h2 className="text-center">Articles</h2>
+      <br />
+      <Container>
+        <div className="articlelist"> </div>
+      </Container>
+      <br />
+      <Footer />
+    </>
+  );
 };
 
 export default CategoryArticles;
