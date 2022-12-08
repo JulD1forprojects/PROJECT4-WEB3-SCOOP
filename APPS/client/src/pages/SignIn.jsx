f8534ed11ecf696be509c5a572b61246fb828e57
Jul-JY <julietta198307@gmail.com>
2022-12-08 17:38:59 +0800

    Rendering SignIn Page

--- APPS/client/src/pages/SignIn.jsx
+++ APPS/client/src/pages/SignIn.jsx
@@ -1,7 +1,27 @@
 import React from "react"; // react
 
 const SignIn = () => {
-  return <div>reference</div>;
+  return (
+    <>
+      <div className="loginbody">
+        <div className="login-page">
+          <div className="loginform">
+            <h3>Login Now</h3>
+            <br />
+            <div className="login-form">
+              <input type="email" placeholder="Email Address" />
+              <input type="password" placeholder="Password" />
+              <button>login</button>
+            </div>
+            <br />
+            <p className="message">
+              Not registered? <Link to="/signup">Create an account</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
