f270ee37f7f95383d67a93501c652e1e323e63ba
Jul-JY <julietta198307@gmail.com>
2022-12-08 17:41:54 +0800

    Rendering NotFound Page

--- APPS/client/src/pages/NotFound.jsx
+++ APPS/client/src/pages/NotFound.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>reference</div>;
+  // rendering ui
+  if (loading) {
+    return (
+      <div className="loadingscreen">
+        <h2 className="mt-2" style={{ fontSize: "45px", color: "white" }}>
+          Loading...
+        </h2>
+        <br />
+      </div>
+    );
+  } else {
+    return (
+      <div className="loadingscreen" style={{ flexDirection: "column" }}>
+        <h2 className="mt-2" style={{ fontSize: "45px", color: "white" }}>
+          Page Not Found
+        </h2>
+        <br />
+        <Link to="/" className="text-decoration-none text-white">
+          Goto Home
+        </Link>
+      </div>
+    );
+  }
 };
 
 export default NotFound;
