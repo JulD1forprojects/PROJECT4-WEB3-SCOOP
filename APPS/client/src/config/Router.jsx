199c33ad553d9e6f781c74d39dd4a569d0385316
Jul-JY <julietta198307@gmail.com>
2022-12-10 00:18:30 +0800

    Updating Router.jsx

--- APPS/client/src/config/Router.jsx
+++ APPS/client/src/config/Router.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // setting app redirection router
+import { useAppContext } from "../context/UseAppContext"; // importing app ontect to get user and loading state
 
 // importing all pages
 import NotFound from "../pages/NotFound";
@@ -13,6 +14,8 @@ import MyArticles from "../pages/MyArticles";
 import Favorites from "../pages/Favorites";
 
 function AppRouter() {
+  const { user, loading } = useAppContext(); // getting user and loading state from app state
+
   // if loading render loading
   if (loading) {
     return (
@@ -25,40 +28,60 @@ function AppRouter() {
       </>
     );
   } else if (!user || !user._id) {
-    // if user is not signed up or logged in - provide all pages except signin and signup page
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" />
-          <Route exact path="/categories" />
-          <Route exact path="/categoryarticles/:category" />
-          <Route exact path="/articles" />
-          <Route exact path="/article/:articleid" />
-          <Route exact path="/favorites" />
-          <Route exact path="/signin" />
-          <Route exact path="/signup" />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/categories" element={<Categories />} />
+          <Route
+            exact
+            path="/categoryarticles/:category"
+            element={<CategoryArticles />}
+          />
+          <Route exact path="/articles" element={<Articles />} />
+          <Route exact path="/article/:articleid" element={<Article />} />
+          <Route exact path="/favorites" element={<Favorites />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
 
-          <Route path="*" />
+          <Route path="*" element={<NotFound loading={loading} />} />
         </Routes>
       </BrowserRouter>
     );
   } else if (user._id) {
-    // if user is logged in - provide all pages except signin and signup page
+    // if user provide all pages except signin and signup page
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" />
-          <Route exact path="/categories" />
-          <Route exact path="/categoryarticles/:category" />
-          <Route exact path="/articles" />
-          <Route exact path="/article/:articleid" />
-          <Route exact path="/favorites" />
-          <Route exact path="/myarticles" />
+          <Route exact path="/" element={<Home user={user} />} />
+          <Route
+            exact
+            path="/categories"
+            element={<Categories user={user} />}
+          />
+          <Route
+            exact
+            path="/categoryarticles/:category"
+            element={<CategoryArticles user={user} />}
+          />
+          <Route exact path="/articles" element={<Articles user={user} />} />
+          <Route
+            exact
+            path="/article/:articleid"
+            element={<Article user={user} />}
+          />
+          <Route exact path="/favorites" element={<Favorites user={user} />} />
+          <Route
+            exact
+            path="/myarticles"
+            element={<MyArticles user={user} />}
+          />
 
-          <Route path="*" />
+          <Route path="*" element={<NotFound loading={loading} />} />
         </Routes>
       </BrowserRouter>
     );
   }
 }
+
 export default AppRouter;
