4527567a46e45c9f46e636e5a415ec5fea52caac
Jul-JY <julietta198307@gmail.com>
2022-12-09 22:00:40 +0800

    Updating Slider Component

--- APPS/client/src/components/Slider.jsx
+++ APPS/client/src/components/Slider.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import landing from "../images/landing.png";
 
 const Slider = () => {
   return (
     <div class="mmmcontainer">
+      <img src={landing} alt="Banner" style={{ width: "100%" }} />
+
       <h2 class="bottom-left text-white">
-        Place to get and share <br /> your latest web3 scoop
+        Place to get and share <br /> your latest Web3 scoop
       </h2>
     </div>
   );
