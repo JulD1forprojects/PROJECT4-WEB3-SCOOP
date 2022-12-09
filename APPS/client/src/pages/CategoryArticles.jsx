8e56e7c745f5cec3c10953f3f1a9ee9d45c44955
Jul-JY <julietta198307@gmail.com>
2022-12-09 23:17:40 +0800

    Updating CategoryArticles Page

--- APPS/client/src/pages/CategoryArticles.jsx
+++ APPS/client/src/pages/CategoryArticles.jsx
@@ -1,16 +1,44 @@
-import React from "react";
-import Header from "../components/Header"; // header
-import Footer from "../components/Footer"; // footer
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import Header from "../components/Header";
+import { useParams } from "react-router-dom";
+import { useAppContext } from "../context/UseAppContext";
+import ArticleCard from "../components/ArticleCard";
+import Footer from "../components/Footer";
 
-const CategoryArticles = () => {
+const CategoryArticles = ({ user }) => {
+  let { category } = useParams(); // get category id from url
+  const [categoryarticles, setCategoryarticles] = useState([]);
+
+  const { articles } = useAppContext(); // get articles from state
+
+  useEffect(() => {
+    console.log(articles);
+    // get category articles
+    var newArray = articles.filter(function (el) {
+      console.log(el);
+      return el.category.category === category;
+    });
+
+    console.log(newArray);
+
+    setCategoryarticles(newArray);
+  }, [articles]);
+
+  // rendering ui
   return (
     <>
-      <Header />
+      <Header user={user} />
       <br />
-      <h2 className="text-center">Articles</h2>
+      <h2 className="text-center">{category} Articles</h2>
       <br />
       <Container>
-        <div className="articlelist"> </div>
+        <div className="articlelist">
+          {/* rendering article card */}
+          {categoryarticles.map((v, i) => {
+            return <ArticleCard key={i} article={v} />;
+          })}
+        </div>
       </Container>
       <br />
       <Footer />
