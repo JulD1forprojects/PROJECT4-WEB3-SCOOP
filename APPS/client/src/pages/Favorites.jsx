fb7e1ba230dc247e62f86787c57f2bc56275cb8c
Jul-JY <julietta198307@gmail.com>
2022-12-10 00:20:23 +0800

    Updating favorites page

--- APPS/client/src/pages/Favorites.jsx
+++ APPS/client/src/pages/Favorites.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import Header from "../components/Header"; // header
+import FavoriteCard from "../components/FavoriteCard"; // article card
+import { Container } from "react-bootstrap"; // conteiner for ui
 import Footer from "../components/Footer"; // footer
+import { useAppContext } from "../context/UseAppContext"; // app contexxt for getting articles
 
-const Favorites = () => {
+const Favorites = ({ user }) => {
+  const { favorites } = useAppContext(); // getting articles
+
+  console.log(favorites);
+
+  // rendering ui
   return (
     <>
-      <Header />
+      <Header user={user} />
       <br />
-      <h2 className="text-center">My Favorites Blogs</h2>
+      <h2 className="text-center">My Favorites Articles</h2>
       <br />
       <Container>
-        <div className="row"></div>
+        <div className="row">
+          {/* reding produt card for all articles */}
+
+          {favorites &&
+            favorites.map((v, i) => {
+              return <FavoriteCard key={i} article={v} />;
+            })}
+        </div>
       </Container>
       <br />
       <Footer />
