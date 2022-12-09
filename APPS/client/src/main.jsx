b523757218e352c1843f791fd2547e0a24c64673
Jul-JY <julietta198307@gmail.com>
2022-12-10 00:17:54 +0800

    Updating main.jsx: importing libraries

--- APPS/client/src/main.jsx
+++ APPS/client/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // imporing bootstrap css
+import "react-toastify/dist/ReactToastify.css"; // importing toastify css
+
+import { ToastContainer } from "react-toastify"; // imporing container for showing the toast message
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
