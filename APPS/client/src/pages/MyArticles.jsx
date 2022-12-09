bccf43df947351015f5272479c5a84a0c6c2ba41
Jul-JY <julietta198307@gmail.com>
2022-12-10 00:37:27 +0800

    Updating My articles page

--- APPS/client/src/pages/MyArticles.jsx
+++ APPS/client/src/pages/MyArticles.jsx
@@ -1,34 +1,80 @@
-import React from "react";
-import Header from "../components/Header"; // header
-import Footer from "../components/Footer"; // footer
+import React, { useState, useEffect } from "react";
+import { Container, Table } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom"; // useparam to get order id from url and usenaviagate to redirect between pages
+import Header from "../components/Header";
+import axios from "axios"; // axios for api calls
+const OrderDetail = ({ user }) => {
+  let { orderid } = useParams(); // get order id
 
-const MyArticles = () => {
-  return (
-    <>
-      <Header />
+  const navigate = useNavigate();
+  const [order, setOrder] = useState({}); // order object
+  const [loading, setLoading] = useState(true); // loading
 
-      <Container>
+  // get order by api
+  const getData = async (id) => {
+    try {
+      const { data } = await axios.get(`/api/v1/order/${id}`);
+      if (data.success === true) {
+        // set order in state
+        setOrder(data.order);
+        setLoading(false);
+      } else {
+        navigate("/myorders");
+      }
+    } catch (e) {
+      console.log(e);
+      navigate("/myorders");
+    }
+  };
+
+  // first run
+  useEffect(() => {
+    getData(orderid);
+  }, [orderid]);
+
+  // rendering ui
+  if (loading) {
+    return (
+      <div className="loadingscreen">
+        <h2 className="mt-2" style={{ fontSize: "45px", color: "white" }}>
+          Loading...
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <Header user={user} />
         <br />
-        <div className="d-flex justify-content-between align-items-center gap-10 flex-wrap-wrap">
-          <h2 className="text-center">My Articles</h2>
-          <button className="btn btn-myprimary">Add New Article</button>
-        </div>
+        <h2 className="text-center"> Order Detail</h2>
+
         <br />
-
-        <div className="row">
-          <p className="description"></p>
-
-          <br />
-          <br />
-          <button className="btn btn-myprimary">Edit</button>
-
-          <button className="btn btn-myprimary">Delete</button>
-        </div>
-      </Container>
-      <br />
-      <Footer />
-    </>
-  );
+        <br />
+        <Container>
+          <Table bordered striped responsive>
+            <thead>
+              <tr>
+                <th>Product Name</th>
+                <th>Product Price</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.orderItems.map((v, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{v.name}</td>
+                    <td>${v.price}</td>
+                    <td>{v.quantity}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Container>
+      </>
+    );
+  }
 };
 
-export default MyArticles;
+export default OrderDetail;
