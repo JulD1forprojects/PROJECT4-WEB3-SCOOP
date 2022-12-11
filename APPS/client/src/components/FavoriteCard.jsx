e421884975dd314a9db2a969d00e0f2eff8e93ab
Jul-JY <julietta198307@gmail.com>
2022-12-11 23:07:08 +0800

    Updating FavouriteCard Component

--- APPS/client/src/components/FavoriteCard.jsx
+++ APPS/client/src/components/FavoriteCard.jsx
@@ -1,21 +1,13 @@
-import React from "react";
-import { useAppContext } from "../context/UseAppContext"; // importing context to get addtocart method
-import { useNavigate } from "react-router-dom"; // to get method for redirection between pages
+import React from "react"; // react
+import { useAppContext } from "../context/UseAppContext"; // importing context to get addToFavorite method
+import { Link } from "react-router-dom"; // to get method for redirection between pages
+const FavoriteArticleCard = ({ article }) => {
+  const { removeFromFavorites } = useAppContext(); // addToFavorite method from context op app
 
-const ArticleCard = ({ article }) => {
-  const { removeFromFavorites } = useAppContext(); // addtocart method from context op app
-  const navigate = useNavigate(); // navigate method for redirection between pages
-
-  // to remove article into favorites
+  //! function to add article into favorites
   const remove = async (id) => {
     console.log(id);
     await removeFromFavorites(id);
-  };
-
-  // redirect to article page function
-  const gotoarticle = (id) => {
-    // navigate to article page with article id
-    navigate(`/article/${id}`);
   };
 
   // rendering ui
@@ -27,7 +19,19 @@ const ArticleCard = ({ article }) => {
           <div className="card-content">
             {/* article description */}
             <h2>{article.title}</h2>
-            <p className="description">{article.description}</p>
+            <p
+              className="description"
+              dangerouslySetInnerHTML={{ __html: article.description }}
+            ></p>
+            <Link
+              to={`/article/${article._id}`}
+              className="text-decoration-none text-black"
+              style={{ fontWeight: "bold" }}
+            >
+              Read More...
+            </Link>
+
+            <br />
             <br />
             <button
               className="btn btn-danger"
@@ -42,4 +46,4 @@ const ArticleCard = ({ article }) => {
   );
 };
 
-export default ArticleCard;
+export default FavoriteArticleCard;
